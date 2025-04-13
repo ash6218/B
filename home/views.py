7101da8e5eef56ed72be3f13c424c2c8aa66e26a
ash6218 <ash6218@gmail.com>
2025-04-13 17:45:23 +0330

    changing local system to Linux!

--- home/views.py
+++ home/views.py
@@ -14,5 +14,5 @@ def detail(request, todo_id):
     return render(request, 'detail.html', {'form2':form2})
 
 def delete(request, todo_id):
-    form2 = Todo.objects.get(id= todo_id).delete()
+    Todo.objects.get(id= todo_id).delete()
     return redirect('home')
